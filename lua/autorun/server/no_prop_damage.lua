6d1bd97922fff2fa999cbb52013706e04c12a297
Unknown Developer <olegis019@gmail.com>
2026-04-04 20:13:05 +0400

    is prop check

--- lua/autorun/server/no_prop_damage.lua
+++ lua/autorun/server/no_prop_damage.lua
@@ -1,10 +1,15 @@
+local string_byte = string.byte
+local bit_band = bit.band
 
 hook.Add( "EntityTakeDamage", "NoMorePropDamage", function( pl, damage_info )
     if not ( pl ~= nil and pl:IsValid() and pl:IsPlayer() and pl:Alive() ) then return end
-    if bit.band( damage_info:GetDamageType(), 1 ) == 0 then return end
+    if bit_band( damage_info:GetDamageType(), 1 ) == 0 then return end
 
     local attacker = damage_info:GetAttacker()
     if not ( attacker ~= nil and attacker:IsValid() ) then return end
+
+    local b1, b2, b3, b4, b5 = string_byte( attacker:GetClass(), 1, 5 )
+	if ( ( ( ( b5 * 0x100 + b4 ) * 0x100 + b3 ) * 0x100 + b2 ) * 0x100 + b1 ) == 0x5f706f7270 --[[ prop_ signature ]] then return end
 
     local phys = attacker:GetPhysicsObject()
     if not ( phys ~= nil and phys:IsValid() ) then return end
